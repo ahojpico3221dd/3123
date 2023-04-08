4a13d6c6ff0251c1f65d54289fa29ef5f2618eb0
ahojpico3221dd <130239599+ahojpico3221dd@users.noreply.github.com>
2023-04-08 18:27:06 +0200

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,11 +1,11 @@
 /**
- * @name NineX
+ * @name Exponent
  * @author Saltssaumure
  * @authorLink https://github.com/Saltssaumure
- * @description A Windows 9x style Discord theme. With support for dark/light mode, custom background image and colour.
- * @version 1.1
+ * @description A Windows XP style theme built for BetterDiscord.
+ * @version 1.3
  * @invite uy8nKQVatp
- * @source https: //github.com/Saltssaumure/w9x-discord-theme
+ * @source https://github.com/Saltssaumure/xp-discord-theme
  * @donate https://ko-fi.com/saltssaumure
 */
 
@@ -20,13 +20,38 @@
                                          Licensed under GPL-3.0
 
 Discord: Saltssaumure#7557
-GitHub: https: //github.com/Saltssaumure/w9x-discord-theme
+GitHub: https://github.com/Saltssaumure/xp-discord-theme
 Join Salt's Discord Themes server: https://discord.gg/uy8nKQVatp
 */
 
-@import url("https://saltssaumure.github.io/w9x-discord-theme/w9x.min.css");
+@import url("https://saltssaumure.github.io/xp-discord-theme/xp.min.css");
 
-:root {
-    --w9x-background-image: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Faesthetic-windows-95-1920x1080--580964420671848735%2F&psig=AOvVaw0bNumS9u9Nqb3x5It3HxX5&ust=1681057075212000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCND80uDXmv4CFQAAAAAdAAAAABAE;
-    --w9x-background-color: #008080;
+:root
+{
+    --background-image: url(https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com%2Fr%2FVaporwave%2Fcomments%2Fb5gbkl%2Fa_tribute_to_our_favorite_windows_xp_default%2F&psig=AOvVaw32BoLRvn0TarXi8Z3kRGOC&ust=1681057464109000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMC8vZrZmv4CFQAAAAAdAAAAABAE);  /* Background image. Values: any image link encased in url(). */
+    --bsod-color: navy;  /* BSOD background colour. Values: any valid keyword/RGBA/hex etc. colour. */
+    --bsod-text: "A problem has been detected and Windows has been shut down to prevent damage to your computer. \A\A A clock interrupt was not received on a secondary processor within the allocated time interval. \A\A If this is the first time you've seen this stop error screen, restart your computer. If this screen appears again, follow these steps: \A\A Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for any Windows updates you might need. \A\A If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then throw away the whole computer. \A\A Technical information: \A\A *** STOP: USING_DISCORD \A\A ***       discord.exe - Address DEADBEEF base at 8BADF00D, DateStamp 1337C0D3 \A\A\A\A\A\A\A\A\A\A Collecting data for crash dump ... \A Initializing disk for crash dump ... \A Beginning dump of all your belongings ... \A Dumping physical memory to disk: 100% \A Physical memory dump complete. \A Contact your system administrator or technical support group for further assistance.";
+}
+
+.theme-dark {
+    --xp-bg-color: #000;
+    --xp-bg-bright-color: #234;
+    --xp-bg-tint-color: #222;
+    --xp-bg-shade-color: #111;
+    --xp-txt-color: #FFF;
+    --xp-border-color: #AE16CA;
+    --xp-set-color: #FFFFFFB3;
+    --xp-hover-color: #123;
+    --xp-zing-color: #A50;
+}
+.theme-light {
+    --xp-bg-color: #FFF;
+    --xp-bg-bright-color: #F0EFED;
+    --xp-bg-tint-color: #EBE8D7;
+    --xp-bg-shade-color: #808080;
+    --xp-txt-color: #000;
+    --xp-border-color: #000;
+    --xp-set-color: #000000B3;
+    --xp-hover-color: #D3D3D3;
+    --xp-zing-color: #FFA500;
 }
