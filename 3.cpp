0ee84e5ca0b03baa601c4b5a4c1c77632651a707
ahojpico3221dd <130239599+ahojpico3221dd@users.noreply.github.com>
2023-04-08 18:20:44 +0200

    ee3E2
    ee

---
+++ 3.cpp
@@ -0,0 +1,32 @@
+/**
+ * @name NineX
+ * @author Saltssaumure
+ * @authorLink https://github.com/Saltssaumure
+ * @description A Windows 9x style Discord theme. With support for dark/light mode, custom background image and colour.
+ * @version 1.1
+ * @invite uy8nKQVatp
+ * @source https: //github.com/Saltssaumure/w9x-discord-theme
+ * @donate https://ko-fi.com/saltssaumure
+*/
+
+/*
+                                                Made by:
+ _______  _______  _    _________ _______  _______  _______           __   __           _______  _______
+(  ____ \(  ___  )( \   \__   __/(  ____ \(  ____ \(  ___  )|\     /|(  \ /  )|\     /|(  ____ )(  ____ \
+| (____\/| (___) || |      | |   | (____\/| (____\/| (___) || |   | || || || || |   | || (____)|| (__  \/
+(_____  )|  ___  || |      | |   (_____  )(_____  )|  ___  || |   | || |(_)| || |   | || __  __)|  __)
+/\____) || )   ( || (____/\| |   /\____) |/\____) || )   ( || (___) || )   ( || (___) || ) \ \__| (____/\
+\_______)|/     \|(_______/)_(   \_______)\_______)|/     \|(_______)|/     \|(_______)|/   \__/(_______/
+                                         Licensed under GPL-3.0
+
+Discord: Saltssaumure#7557
+GitHub: https: //github.com/Saltssaumure/w9x-discord-theme
+Join Salt's Discord Themes server: https://discord.gg/uy8nKQVatp
+*/
+
+@import url("https://saltssaumure.github.io/w9x-discord-theme/w9x.min.css");
+
+:root {
+    --w9x-background-image: https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Faesthetic-windows-95-1920x1080--580964420671848735%2F&psig=AOvVaw0bNumS9u9Nqb3x5It3HxX5&ust=1681057075212000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCND80uDXmv4CFQAAAAAdAAAAABAE;
+    --w9x-background-color: #008080;
+}
